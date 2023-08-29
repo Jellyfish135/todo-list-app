059e570cd1afeb5281e2e674e6b883bc2645b9c1
Kevin Lee <kevinlee@Kevins-MacBook-Air.local>
2023-08-29 23:50:48 +0900

    [1/n] Create TodoItem react component: Create a placeholder component & display todo item

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
-import "./App.css"
-import { useState } from "react"
-import TextField from "@mui/material/TextField"
-import Button from "@mui/material/Button"
+import "./App.css";
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
-let todoItemId = 0
+let todoItemId = 0;
 
 const TodoItemInputField = (props) => {
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState("");
 
   const onSubmit = () => {
-    props.onSubmit(input)
-    setInput("")
-  }
+    props.onSubmit(input);
+    setInput("");
+  };
 
-  //console.log(input)
   return (
     <div>
       <TextField
-        id="todo-item -input"
+        id="todo-item-input"
         label="Todo Item"
         variant="outlined"
         onChange={(e) => setInput(e.target.value)}
         value={input}
       />
-
       <Button variant="outlined" onClick={onSubmit}>
         Submit
       </Button>
     </div>
-  )
-}
+  );
+};
+
+const TodoItem = (props) => {
+  return (
+    <li>
+      <span>{props.todoItem.todoItemContent}</span>
+    </li>
+  );
+};
 
 const TodoItemList = (props) => {
   const todoList = props.todoItemList.map((todoItem, index) => {
-    return <li key={index}>{todoItem.todoItemContent}</li>
-  })
-
+    return <TodoItem key={index} todoItem={todoItem} />;
+  });
   return (
     <div>
       <ul>{todoList}</ul>
     </div>
-  )
-}
+  );
+};
+
 function App() {
-  const [todoItemList, setTodoItemList] = useState([])
+  const [todoItemList, setTodoItemList] = useState([]);
 
   const onSubmit = (newTodoItem) => {
     setTodoItemList([
@@ -53,15 +59,15 @@ function App() {
         todoItemContent: newTodoItem,
         isFinished: false,
       },
-    ])
-  }
+    ]);
+  };
 
   return (
     <div className="App">
       <TodoItemInputField onSubmit={onSubmit} />
       <TodoItemList todoItemList={todoItemList} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
