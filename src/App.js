9e6e401a184c33bc1cc3481b7d15429321b443bd
Kevin Lee <kevinlee@Kevins-MacBook-Air.local>
2023-08-30 20:59:32 +0900

    Add todo item to Firestore when new todo item is added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Button from "@mui/material/Button";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,7 +26,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-let todoItemId = 0;
+const db = getFirestore(app);
 
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
@@ -91,11 +92,16 @@ const TodoItemList = (props) => {
 function App() {
   const [todoItemList, setTodoItemList] = useState([]);
 
-  const onSubmit = (newTodoItem) => {
+  const onSubmit = async (newTodoItem) => {
+    const docRef = await addDoc(collection(db, "todoItem"), {
+      todoItemContent: newTodoItem,
+      isFinished: false,
+    });
+
     setTodoItemList([
       ...todoItemList,
       {
-        id: todoItemId++,
+        id: docRef.id,
         todoItemContent: newTodoItem,
         isFinished: false,
       },
