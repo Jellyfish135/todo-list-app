1ef4c332d4b01d9d9b11a236ec239790f4c1148c
Kevin Lee <kevinlee@Kevins-MacBook-Air.local>
2023-09-01 15:30:40 +0900

    Refactor data flow

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ const TodoItemList = (props) => {
 
 function App() {
   const [todoItemList, setTodoItemList] = useState([]);
-  useEffect(() => {
+  const syncTodoItemListStateWithFirestore = () => {
     getDocs(collection(db, "todoItem")).then((querySnapshot) => {
       const firestoreTodoItemList = [];
       querySnapshot.forEach((doc) => {
@@ -110,21 +110,18 @@ function App() {
       });
       setTodoItemList(firestoreTodoItemList);
     });
+  };
+
+  useEffect(() => {
+    syncTodoItemListStateWithFirestore();
   }, []);
 
   const onSubmit = async (newTodoItem) => {
-    const docRef = await addDoc(collection(db, "todoItem"), {
+    await addDoc(collection(db, "todoItem"), {
       todoItemContent: newTodoItem,
       isFinished: false,
     });
-    setTodoItemList([
-      ...todoItemList,
-      {
-        id: docRef.id,
-        todoItemContent: newTodoItem,
-        isFinished: false,
-      },
-    ]);
+    syncTodoItemListStateWithFirestore();
   };
 
   const onTodoItemClick = async (clickedTodoItem) => {
@@ -134,30 +131,14 @@ function App() {
       { isFinished: !clickedTodoItem.isFinished },
       { merge: true }
     );
-    setTodoItemList(
-      todoItemList.map((todoItem) => {
-        if (clickedTodoItem.id === todoItem.id) {
-          return {
-            id: clickedTodoItem.id,
-            todoItemContent: clickedTodoItem.todoItemContent,
-            isFinished: !clickedTodoItem.isFinished,
-          };
-        } else {
-          return todoItem;
-        }
-      })
-    );
+    syncTodoItemListStateWithFirestore();
   };
 
   const onRemoveClick = async (removedTodoItem) => {
     const todoItemRef = doc(db, "todoItem", removedTodoItem.id);
     await deleteDoc(todoItemRef);
 
-    setTodoItemList(
-      todoItemList.filter((todoItem) => {
-        return todoItem.id !== removedTodoItem.id;
-      })
-    );
+    syncTodoItemListStateWithFirestore();
   };
 
   return (
